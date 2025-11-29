04d69e88c336179dc2e9cc76d8fb1074a08fb397
DRMO <inedumozey@gmail.com>
2025-11-29 18:45:23 +0100

    updated

--- src/app/api/mormDocs.ts
+++ src/app/api/mormDocs.ts
@@ -14,6 +14,7 @@ export class MormDocs {
   private async connect(dbUrl?: string) {
     this.client = new Client({
       connectionString: dbUrl || this.databaseUrl,
+      ssl: { rejectUnauthorized: true },
     });
     await this.client.connect();
   }
